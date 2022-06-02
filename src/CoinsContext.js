db9b1146d1fa3dcbbb8289c309c129d20851093c
ericthiessen01 <ericthiessen01@gmail.com>
2022-06-01 20:06:59 -0600

    Fixed issue with portfolio data not updating

--- src/CoinsContext.js
+++ src/CoinsContext.js
@@ -6,7 +6,7 @@ function ContextProvider({children}) {
     const [topHundred, setTopHundred] = React.useState([])
     const [portfolioItems, setPortfolioItems] = React.useState(() => {
         const storedPortfolio = localStorage.getItem('portfolio')
-        return storedPortfolio !== null
+        return storedPortfolio !== null || undefined
             ? JSON.parse(storedPortfolio)
             : [];
       })
@@ -15,15 +15,23 @@ function ContextProvider({children}) {
     function getData() {
         fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
             .then(res => res.json())
-            .then(data => setTopHundred(data))
+            .then(data => {
+                setTopHundred(data)
+                setPortfolioItems(prevPortfolioItems => 
+                    data.filter(updatedData => 
+                        prevPortfolioItems.some(oldData => 
+                            updatedData.id === oldData.id)
+                    )
+                )
+            })
     }
 
 //run fetch and update state every hour
     React.useEffect(() => {
         getData()
         const interval = setInterval(() => {
-        getData()
-            }, 3600000)
+            getData()
+        }, 3600000)
         return () => clearInterval(interval)
     }, [])
     // console.log(topHundred)
@@ -31,7 +39,7 @@ function ContextProvider({children}) {
 //function to add a coin to portfolio
     function addToPortfolio(selectedPortfolioItem) {
         setPortfolioItems(prevPortfolioItems => {
-            if(portfolioItems.some(asset => asset.id === selectedPortfolioItem.id)){
+            if(prevPortfolioItems.some(asset => asset.id === selectedPortfolioItem.id)){
                 return (
                     prevPortfolioItems
                 )
@@ -46,7 +54,7 @@ function ContextProvider({children}) {
 
     React.useEffect(() => {
         localStorage.setItem('portfolio', JSON.stringify(portfolioItems))
-        console.log(portfolioItems)
+        // console.log(portfolioItems)
     }, [portfolioItems])
 
     return (
